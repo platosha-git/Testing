bd6b3857ddd4525033a819b76b1a5366c2285070
platosha <15platosha@gmail.com>
2022-01-04 02:31:31 +0300

    done transfer

---
+++ source/Tours/ToursTests/IntegrationTests/TransferTests.cs
@@ -0,0 +1,117 @@
+using Xunit;
+using ToursWeb.ModelsDB;
+using ToursWeb.ModelsBL;
+using ToursTests.Builders;
+using System.Collections.Generic;
+
+namespace ToursTests.IntegrationTests
+{
+    public class TransferTests : IClassFixture<TourAccessObject>
+    {
+        private readonly TourAccessObject _accessObject;
+
+        public TransferTests(TourAccessObject tourAccessObject) 
+        {
+            _accessObject = tourAccessObject;
+        }
+
+        [Fact]
+        public void FindAll_NotNull()
+        {
+            // Arrange
+            var expTransf = createTransfList();
+            addEntities(expTransf);
+
+            // Act
+            var actTransfBL = _accessObject.transferRepository.FindAll();
+            var actTransf = getTransfList(actTransfBL);
+
+            // Assert
+            Assert.NotNull(actTransf);
+            Assert.Equal(expTransf.Count, actTransf.Count);
+            Assert.True(areEqual(expTransf, actTransf));
+
+            Cleanup();
+        }
+
+        [Fact]
+        public void FindByID_FirstElement_NotNull()
+        {
+            const int transferID = 1;
+            
+            // Arrange
+            var expTransf = createTransfList();
+            addEntities(expTransf);
+
+            // Act
+            var actTransfBL = _accessObject.transferRepository.FindByID(transferID);
+            var actTransf = new Transfer(actTransfBL);
+
+            // Assert
+            Assert.NotNull(actTransf);
+            Assert.Equal(transferID, actTransf.Transferid);
+
+            Cleanup();
+        }
+
+        private List<Transfer> createTransfList()
+        {
+            var transfers = new List<Transfer>();
+            for (var i = 1; i < 5; i++)
+            {
+                var curTransferBL = new TransferBuilder()
+                    .WhereTransferID(i)
+                    .Build();
+                var curTransfer = new Transfer(curTransferBL);
+                transfers.Add(curTransfer);
+            }
+            
+            return transfers;
+        }
+
+        void addEntities(List<Transfer> transfers)
+        {
+            _accessObject.toursContext.ChangeTracker.Clear();
+            _accessObject.toursContext.Transfers.AddRange(transfers);
+            _accessObject.toursContext.SaveChanges();
+        }
+
+        private List<Transfer> getTransfList(List<TransferBL> transfBL)
+        {
+            List<Transfer> transfers = new List<Transfer>();
+            foreach (var transBL in transfBL)
+            {
+                Transfer transfer = new Transfer(transBL);
+                transfers.Add(transfer);
+            }
+            return transfers;
+        }
+
+        bool areEqual(List<Transfer> expTransf, List<Transfer> actTransf)
+        {
+            bool equal = true;
+            for (int i = 0; i < expTransf.Count && equal; i++)
+            {
+                equal = areEqual(expTransf[i], actTransf[i]);
+            }
+            
+            return equal;
+        }
+        
+        bool areEqual(Transfer expTransf, Transfer actTransf)
+        {
+            return (expTransf.Transferid == actTransf.Transferid &&
+                    expTransf.Type == actTransf.Type &&
+                    expTransf.Cityfrom == actTransf.Cityfrom &&
+                    expTransf.Departuretime == actTransf.Departuretime &&
+                    expTransf.Cost == actTransf.Cost);
+        }
+
+        void Cleanup()
+        {
+            _accessObject.toursContext.ChangeTracker.Clear();
+            _accessObject.toursContext.Transfers.RemoveRange(_accessObject.toursContext.Transfers);
+            _accessObject.toursContext.SaveChanges();
+        }
+    }
+}
